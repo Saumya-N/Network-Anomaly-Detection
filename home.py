ea29c30f55a574fb16e19d26d521880951d01b5f
Saumya Nishi <saumya.nishi96@gmail.com>
2024-08-05 09:22:53 +0530

    Update home.py

--- home.py
+++ home.py
@@ -8,4 +8,15 @@ def home_page():
     st.write('Welcome to the Network Anomaly Detection App!')
 
     # Embed video
-    st.video('https://www.loom.com/share/188dd624e4a6498b9c4e488a2519a31c?sid=97ae0d87-fd27-4df6-93d1-a305e55b47c6')
+    # Embed Loom video
+    st.markdown("""
+    <div style="position: relative; padding-bottom: 64.98194945848375%; height: 0;">
+        <iframe src="https://www.loom.com/embed/188dd624e4a6498b9c4e488a2519a31c?sid=c200d6e9-e37e-486c-bb42-391e9e72f39f" 
+                frameborder="0" 
+                webkitallowfullscreen 
+                mozallowfullscreen 
+                allowfullscreen 
+                style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;">
+        </iframe>
+    </div>
+    """, unsafe_allow_html=True)
