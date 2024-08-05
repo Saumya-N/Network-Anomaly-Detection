858b21278921cd15731ea540f57ec65e6ab66504
Saumya Nishi <saumya.nishi96@gmail.com>
2024-08-05 09:18:21 +0530

    Video embedding

--- home.py
+++ home.py
@@ -6,3 +6,6 @@ def home_page():
     st.image('bkg.png', use_column_width=True)
     st.title('Home')
     st.write('Welcome to the Network Anomaly Detection App!')
+
+    # Embed video
+    st.video('https://www.loom.com/share/188dd624e4a6498b9c4e488a2519a31c?sid=b8c b17b3-e84b-49de-976e-1d598fa4968b')
